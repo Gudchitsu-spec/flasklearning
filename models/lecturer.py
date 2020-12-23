42e87956d6c21ea361e42ff81c1186059ffee4f1
Gudchitsu <gudus.mag@ya.ru>
2020-12-24 01:08:55 +0300

    Initial commit

---
+++ models/lecturer.py
@@ -0,0 +1,9 @@
+import extensions
+from extensions import db
+
+class Lecturer(db.Model):
+    __tablename__ = 'lecturer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    surname = db.Column(db.String(30), nullable=False)
